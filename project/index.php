bbc9493ee715c933aaa23b9f31873900a126e3bc
itsmanju-git <136497863+itsmanju-git@users.noreply.github.com>
2023-07-16 15:12:48 +0545

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Skill Finder</title>
+    </head>
+    <body>
+
+        <h1>Skill Finder</h1>
+        <h2>Host Programming</h2>
+    </body>
+</html>
